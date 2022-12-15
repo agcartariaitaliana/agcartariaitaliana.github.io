2b3278411393d4cd3a4f17500b77f0d3df2ca34d
agcartariaitaliana <agallesi@cartariaitaliana.it>
2022-12-15 10:35:35 +0100

    Reorganized folders

--- JS/header.js
+++ JS/header.js
@@ -3,7 +3,7 @@ class AppHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <a href="/index.html" class="nav__menu--left">
-            <img style="width: 128px; height: auto" src="/CI_CSS/Images/liit_logo.svg" alt="Logo LiiT">
+            <img style="width: 128px; height: auto" src="/Images/liit.svg" alt="Logo LiiT">
         </a>
         <div class="nav__menu--right">
             <a href="javascript:history.back()">Indietro</a>
