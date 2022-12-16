e9bb7ced29ee26733f1c30668d33518dafcccd21
agcartariaitaliana <agallesi@cartariaitaliana.it>
2022-12-16 10:34:24 +0100

    correction to footer and header

--- JS/footer.js
+++ JS/footer.js
@@ -1,9 +1,9 @@
 class AppFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            <h2>Scopri i nostri <a class="typography__title--accent" href="/prodotti.html">prodotti</a> e il nostro <a class="typography__title--accent" href="/servizi.html">servizio</a> di dropshipping.</h1>
+            <h3>Scopri i nostri <a class="typography__title--accent" href="/prodotti.html">prodotti</a> e il nostro <a class="typography__title--accent" href="/servizi.html">servizio</a> di dropshipping.</h3>
             <br>
-            <h2>Hai altre domande? Scrivici un’email a <a class="typography__title--accent" href="mailto:info@cartariaitaliana.it">info@cartariaitaliana.it</a> o telefonaci allo <a class="typography__title--accent" href="tel:023542189">02 354 2189</a></h1>
+            <h3>Hai altre domande? Scrivici un’email a <a class="typography__title--accent" href="mailto:info@cartariaitaliana.it">info@cartariaitaliana.it</a> o telefonaci al numero <a class="typography__title--accent" href="tel:023542189">+39 02 354 2189</a></h3>
             <br>
             <p>LiiT srl via Giotto, 28 20032 Cormano (Milano) - Italia</p>
         `
--- JS/header.js
+++ JS/header.js
@@ -4,9 +4,10 @@ class AppHeader extends HTMLElement {
         <a href="/index.html" class="nav__menu--left">
             <img style="width: 128px; height: auto" src="/Images/liit.svg" alt="Logo LiiT">
         </a>
-        <div class="nav__menu--right">
-            <a href="javascript:history.back()">Indietro</a>
-        </div>
+        <ul class="nav__menu--right">
+            <li class="corners button button__small button__tertiary"><a href="/prodotti.html">Prodotti</a></li>
+            <li class="corners button button__small button__tertiary"><a href="/servizi.html">Servizi</a></li>
+        </ul>
         `
     }
 }
